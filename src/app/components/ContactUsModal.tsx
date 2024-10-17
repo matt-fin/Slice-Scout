eab0902d51a2083fb6faf16d6037695e63a84614
Rei Zheng <joannazheng13@gmail.com>
2024-10-17 17:43:32 -0400

    updated contactusmodal for form submission
    Updated contactusmodal for form submission

--- src/app/components/ContactUsModal.tsx
+++ src/app/components/ContactUsModal.tsx
@@ -8,34 +8,50 @@ import {
   ModalFooter,
   ModalBody,
   ModalCloseButton,
-  useDisclosure,
   FormControl,
   FormLabel,
   Input,
   Textarea,
   Button,
-  Image,
+  Box,
+  Text
 } from "@chakra-ui/react"
 import { useState } from "react"
 
 export default function ContactUsModal({isOpen, onClose}) {
 
-  //const { isOpen, onOpen, onClose } = useDisclosure();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  
+  const handleFormSubmit = async (e) => {
+      e.preventDefault();
+      setIsSubmitted(true);
+  };
+
+  const handleFormClosure = () => {
+    setName("");
+    setEmail("");
+    setMessage("");
+    setIsSubmitted(false);
+    onClose();
+  }
   
   return (
     
     <Modal 
       blockScrollOnMount={false} 
       isOpen={isOpen} 
-      onClose={onClose} 
+      onClose={handleFormClosure} 
       size="sm" 
       isCentered={false}
       closeOnOverlayClick={false}>
       <ModalOverlay bg="transparent"/>
       <ModalContent 
         position="fixed"
-        maxWidth="35vw" 
-        minWidth="25vw"
+        maxWidth={{ base: "60vw", md: "35vw", lg: "40vw" }} 
+        minWidth={{ base: "40vw", md: "25vw", lg: "30vw" }}
         width="100%"
         bottom="35px"
         right="30px"
@@ -50,28 +66,40 @@ export default function ContactUsModal({isOpen, onClose}) {
         <ModalCloseButton />
         <ModalBody
           paddingTop="90px"
+          paddingBottom="60px"
         >
-          <FormControl id="name" mb={4}>
-            <FormLabel>Name</FormLabel>
-            <Input type="text" placeholder="Your Name" />
-          </FormControl>
+          {isSubmitted ? (
+            <Box textAlign="center">
+              <Text fontSize="30px">Thank You, {name}!</Text>
+              <br/>
+              <p>Your message has been received. We will get back to you within 2 business days.</p>
+            </Box>
+          ) : (
+          <form onSubmit={handleFormSubmit}>
+            <FormControl id="name" mb={4} isRequired>
+              <FormLabel>Name</FormLabel>
+              <Input type="text" placeholder="Your Name" value={name}
+                onChange={(e) => setName(e.target.value)}/>
+            </FormControl>
           
-          <FormControl id="email" mb={4}>
-            <FormLabel>Email</FormLabel>
-            <Input type="email" placeholder="Your Email" />
-          </FormControl>
+            <FormControl id="email" mb={4} isRequired>
+              <FormLabel>Email</FormLabel>
+              <Input type="email" placeholder="Your Email" value={email}
+                onChange={(e) => setEmail(e.target.value)} />
+            </FormControl>
           
-          <FormControl id="message" mb={4}>
-            <FormLabel>Message</FormLabel>
-            <Textarea placeholder="Your Message" />
-          </FormControl>
+            <FormControl id="message" mb={4} isRequired>
+              <FormLabel>Message</FormLabel>
+              <Textarea placeholder="Your Message" value={message}
+                onChange={(e) => setMessage(e.target.value)} />
+            </FormControl>
+            <ModalFooter>
+              <Button colorScheme="orange" type="submit" marginTop="30px">
+              Submit
+              </Button>
+            </ModalFooter>
+          </form>)}
         </ModalBody>
-
-        <ModalFooter>
-          <Button colorScheme="orange" mr={3} onClick={onClose}>
-            Submit
-          </Button>
-        </ModalFooter>
       </ModalContent>
     </Modal>
   );
